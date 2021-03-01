c4f2e4cdaf21e162376ebbec980ea58264365f61
Denis Kaydunov <deniskaydunov@gmail.com>
2021-03-01 10:23:49 +0300

    Add logic Files. Fix mistake name of Variable in logic files.

--- chatbothospital/src/main/java/by/jrr/chatbothospital/logic/User.java
+++ chatbothospital/src/main/java/by/jrr/chatbothospital/logic/User.java
@@ -2,19 +2,20 @@ package by.jrr.chatbothospital.logic;
 
 /**
  * User - пациент
- * isBloking - заблокирован ли пользователь в системе
+ * isBlocking - заблокирован ли пользователь в системе
  */
 public class User {
     private String name;
-    private String surename;
+    private String surname;
     private String password;
-    //private int year;
-    //private String adress;
-    private boolean isBloking = false;
+    private boolean isBlocking = false;
+    private int year;
+    private String address;
+
 
     public User(String name, String surename, String password) {
         this.name = name;
-        this.surename = surename;
+        this.surname = surename;
         this.password = password;
     }
 
@@ -26,12 +27,12 @@ public class User {
         this.name = name;
     }
 
-    public String getSurename() {
-        return surename;
+    public String getSurname() {
+        return surname;
     }
 
-    public void setSurename(String surename) {
-        this.surename = surename;
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getPassword() {
@@ -42,11 +43,58 @@ public class User {
         this.password = password;
     }
 
-    public boolean isBloking() {
-        return isBloking;
+    public boolean isBlocking() {
+        return isBlocking;
     }
 
-    public void setBloking(boolean bloking) {
-        isBloking = bloking;
+    public void setBlocking(boolean blocking) {
+        isBlocking = blocking;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (isBlocking() != user.isBlocking()) return false;
+        if (year != user.year) return false;
+        if (getName() != null ? !getName().equals(user.getName()) : user.getName() != null) return false;
+        if (getSurname() != null ? !getSurname().equals(user.getSurname()) : user.getSurname() != null) return false;
+        if (getPassword() != null ? !getPassword().equals(user.getPassword()) : user.getPassword() != null)
+            return false;
+        return address != null ? address.equals(user.address) : user.address == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getSurname() != null ? getSurname().hashCode() : 0);
+        result = 31 * result + (getPassword() != null ? getPassword().hashCode() : 0);
+        result = 31 * result + (isBlocking() ? 1 : 0);
+        result = 31 * result + year;
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", isBlocking=" + isBlocking +
+                ", year=" + year +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
