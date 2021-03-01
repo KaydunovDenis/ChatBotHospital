b149908b919c10e5317d0cf31b0697069b2196c6
Denis Kaydunov <deniskaydunov@gmail.com>
2021-03-01 10:26:53 +0300

    Add logic Files. Fix mistake name of Variable in logic files. Add toString, equal and  hashCode

--- chatbothospital/src/main/java/by/jrr/chatbothospital/logic/Order.java
+++ chatbothospital/src/main/java/by/jrr/chatbothospital/logic/Order.java
@@ -9,19 +9,38 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * }
  */
 public class Order {
-    final private Spesialist SPECIALIST;
+    final private Specialist SPECIALIST;
     private CopyOnWriteArrayList listUsersOnOrder;//Нужно использовать потоко безопасную очередь
 
-    public Order(Spesialist spesialist) {
-        SPECIALIST = spesialist;
+    public Order(Specialist specialist) {
+        SPECIALIST = specialist;
         listUsersOnOrder = new CopyOnWriteArrayList();
     }
 
-    public Spesialist getSpesialist() {
+    public Specialist getSpesialist() {
         return SPECIALIST;
     }
 
     public CopyOnWriteArrayList getListUsersOnOrder() {
         return listUsersOnOrder;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+
+        Order order = (Order) o;
+
+        if (SPECIALIST != null ? !SPECIALIST.equals(order.SPECIALIST) : order.SPECIALIST != null) return false;
+        return getListUsersOnOrder() != null ? getListUsersOnOrder().equals(order.getListUsersOnOrder()) : order.getListUsersOnOrder() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = SPECIALIST != null ? SPECIALIST.hashCode() : 0;
+        result = 31 * result + (getListUsersOnOrder() != null ? getListUsersOnOrder().hashCode() : 0);
+        return result;
     }
 }
